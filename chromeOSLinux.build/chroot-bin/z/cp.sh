babd32cda44e025a9847b60af75630aa66e7815e
shaynnbabe <shaynababe3@gmail.com>
2022-12-24 10:01:04 -0800

    newupdate

---
+++ chromeOSLinux.build/chroot-bin/z/cp.sh
@@ -0,0 +1,12 @@
+cp crouton-noroot chromeOSLinux-noroot
+cp croutonfindnacl chromeOSLinuxfindnacl 
+cp croutonurlhandler chromeOSLinuxlhandler
+cp crouton-unity-autostart chromeOSLinux-unity-autostart 
+cp croutonnotify chromeOSLinuxnotify
+cp croutonversion chromeOSLinuxversion
+cp croutonclip chromeOSLinuxclip
+cp croutonpowerd chromeOSLinuxpowerd
+cp croutonxinitrc-wrapper chromeOSLinuxxinitrc-wrapper
+cp croutoncycle chromeOSLinuxcycle
+cp croutontriggerd chromeOSLinuxtriggerd
+cp croutonurlhandler chromeOSLinuxurlhandler
