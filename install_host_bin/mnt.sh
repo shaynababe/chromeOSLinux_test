43c389c60046eca38a4410964ee93faa289428e1
shaynababe <shaynababe3@gmail.com>
2023-01-23 09:55:55 -0800

    Mon Jan 23 09:53:29 PST 2023

--- install_host_bin/mnt.sh
+++
@@ -1,5 +0,0 @@
-sudo mount /dev/mmcblk1 /home/SD
-sudo mount -o remount,symfollow -o exec,dev /home/SD/
-sudo mount -o remount,symfollow -o exec,dev /tmp    
-sudo mount -o remount,symfollow -o exec,dev /var/run
-sudo mount -o remount,symfollow -o exec,dev /usr/local
